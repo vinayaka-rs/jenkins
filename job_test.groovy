9aa3efab6f77db1e4c9019c1b5ab9cc1deadd5d0
vinayaka-rs <vinayaka_rs@outlook.com>
2019-10-04 18:48:05 +0530

    test pipeline added

--- job_test.groovy
+++ job_test.groovy
@@ -1,4 +1,4 @@
-job('example') {
+job('Spring-boot-build') {
     scm {
         git {
             remote {
@@ -15,4 +15,30 @@ job('example') {
         gradle('clean build')
     }
 
+    publishers {
+        downstreamParameterized {
+            trigger('Spring-boot-test') {
+                condition('SUCCESS')
+            }
+        }
+    }
+
+}
+
+job('Spring-boot-test') {
+    scm {
+        git {
+            remote {
+                url("https://github.com/vinayaka-rs/spring-boot-gradle.git")
+                credentials("605abf89-a797-4b53-ba81-427f3b29a12d")
+            }
+            extensions {
+                wipeOutWorkspace()
+            }
+            branch('master')
+        }
+    }
+    steps {
+        gradle('clean test')
+    }
 }
