cb4d382540a7af477abb3fc50dbf33c23ea794e5
vinayaka-rs <vinayaka_rs@outlook.com>
2019-10-14 11:52:57 +0530

    email notification added

--- job_test.groovy
+++ job_test.groovy
@@ -14,6 +14,33 @@ job('Spring-boot-build') {
     steps {
         gradle('sonarqube')
         gradle('clean build')
+        publishers { 
+            mailer('vinayaka_rs@outlook.com', true, true) 
+            extendedEmail { recipientList('vinayaka_rs@outlook.com') 
+            defaultSubject('$PROJECT_NAME - Build # $BUILD_NUMBER - $BUILD_STATUS!') 
+            defaultContent('$PROJECT_NAME - Build # $BUILD_NUMBER - $BUILD_STATUS:\n' + '\n' + 'Check console output at $BUILD_URL to view the results.') 
+            contentType('text/html') 
+            triggers { 
+                failure { 
+                    replyToList('$PROJECT_DEFAULT_REPLYTO') 
+                    contentType('text/html') 
+                    content('$PROJECT_DEFAULT_CONTENT') 
+                    subject('$PROJECT_DEFAULT_SUBJECT') 
+                    sendTo { 
+                        recipientList() 
+                    } 
+                } 
+                success { 
+                    replyToList('$PROJECT_DEFAULT_REPLYTO') 
+                    contentType('text/html') 
+                    content('$PROJECT_DEFAULT_CONTENT') 
+                    subject('$PROJECT_DEFAULT_SUBJECT') 
+                    sendTo { 
+                        recipientList() 
+                    } 
+                } 
+            } 
+        }
     }
 
     publishers {
