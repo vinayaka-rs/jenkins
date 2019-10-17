d22a76e959552f87c586534757ea09484129d88d
vinayaka-rs <vinayaka_rs@outlook.com>
2019-10-17 14:42:13 +0530

    modifing pipeline dsl

--- job_stage.groovy
+++ job_stage.groovy
@@ -1,21 +1,19 @@
-pipeline {
-    job('pipeline-dsl-example') {
-        stages {
-            stage('Build And Deploy') {
-                steps {
-                    checkout([
-                        $class: 'GitSCM',
-                        branches: [[name: '*/master']],
-                        userRemoteConfigs: [[url: 'https://github.com/vinayaka-rs/spring-boot-gradle.git',
-                        credentialsId: '605abf89-a797-4b53-ba81-427f3b29a12d']],
-                        poll: true,
-                        interval: '* * * * *']
-                        )
-                    
-                    gradle('clean build')
-                }
-    
+pipelineJob('pipeline-dsl-example') {
+    stages {
+        stage('Build And Deploy') {
+            steps {
+                checkout([
+                    $class: 'GitSCM',
+                    branches: [[name: '*/master']],
+                    userRemoteConfigs: [[url: 'https://github.com/vinayaka-rs/spring-boot-gradle.git',
+                    credentialsId: '605abf89-a797-4b53-ba81-427f3b29a12d']],
+                    poll: true,
+                    interval: '* * * * *']
+                    )
+                
+                gradle('clean build')
             }
+
         }
     }
 }
