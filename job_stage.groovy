a7ac571528d7866e7f29d3233c0af230cd96d8c8
vinayaka-rs <vinayaka_rs@outlook.com>
2019-10-18 12:25:06 +0530

    added stage view

--- job_stage.groovy
+++ job_stage.groovy
@@ -1,24 +1,47 @@
 pipelineJob('pipeline-stage') {
 //    agent any
-    job('Git') {
-        deliveryPipelineConfiguration('Git', 'git clone')
-        scm {
-            git {
-                remote {
-                    url("https://github.com/vinayaka-rs/spring-boot-gradle.git")
-                    credentials("605abf89-a797-4b53-ba81-427f3b29a12d")
+    stages {
+        stage('git') {
+            steps {
+                scm {
+                    git {
+                        remote {
+                            url("https://github.com/vinayaka-rs/spring-boot-gradle.git")
+                            credentials("605abf89-a797-4b53-ba81-427f3b29a12d")
+                        }
+                        extensions {
+                            wipeOutWorkspace()
+                        }
+                        branch('master')
+                    }
                 }
-                extensions {
-                    wipeOutWorkspace()
-                }
-                branch('master')
+            }
+        }
+        stage('build') {
+            steps {
+                gradle('clean build')
             }
         }
     }
-    job('Build') {
-        deliveryPipelineConfiguration('build')
-        steps {
-            gradle('clean build')
-        }
-    }
+//    job('Git') {
+//        deliveryPipelineConfiguration('Git', 'git clone')
+//        scm {
+//            git {
+//                remote {
+//                    url("https://github.com/vinayaka-rs/spring-boot-gradle.git")
+ //                   credentials("605abf89-a797-4b53-ba81-427f3b29a12d")
+//                }
+//                extensions {
+//                    wipeOutWorkspace()
+//                }
+//                branch('master')
+//            }
+//        }
+ //   }
+//    job('Build') {
+//        deliveryPipelineConfiguration('build')
+//        steps {
+//            gradle('clean build')
+//        }
+//    }
 }
