7070dad8cdbcacc1e83d6e3d2ee1184c2aba8d4c
vinayaka-rs <vinayaka_rs@outlook.com>
2019-10-18 11:39:15 +0530

    added pipelinejob method

--- job_stage.groovy
+++ job_stage.groovy
@@ -1,20 +1,21 @@
-pipeline {
+pipelineJob('pipeline-stage') {
     agent any
-    stages {
-        stage('Build And Deploy') {
-            steps {
-                checkout([
-                    $class: 'GitSCM',
-                    branches: [[name: '*/master']],
-                    userRemoteConfigs: [[url: 'https://github.com/vinayaka-rs/spring-boot-gradle.git',
-                    credentialsId: '605abf89-a797-4b53-ba81-427f3b29a12d']],
-                    poll: true,
-                    interval: '* * * * *']
-                    )
-                
-                gradle('clean build')
-            }
-
+    job('Git') {
+        deliveryPipelineConfiguration('Git', 'git clone')
+        steps {
+            checkout([
+                $class: 'GitSCM',
+                branches: [[name: '*/master']],
+                userRemoteConfigs: [[url: 'https://github.com/vinayaka-rs/spring-boot-gradle.git', credentialsId: '605abf89-a797-4b53-ba81-427f3b29a12d']],
+                poll: true,
+                interval: '* * * * *']
+            )
+        }
+    }
+    job('Build') {
+        deliveryPipelineConfiguration('build')
+        steps {
+            gradle('clean build')
         }
     }
 }
